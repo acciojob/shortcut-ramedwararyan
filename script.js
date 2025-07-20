fe8f925abc85d7515d5910a62bdc16c9f88724e0
ramedwararyan <143419537+ramedwararyan@users.noreply.github.com>
2025-07-21 01:31:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function shortcut(s1, s2) {
   // your code here
-	 if (str1.length === 0 || str2.length === 0) {
-    return "";
+	 if (!s1 || !s2) {
+    return '';
   }
-  return str1[0] + str2[0];
+  return s1[0] + s2[0];
 }
 
 // Do not change the code below.
